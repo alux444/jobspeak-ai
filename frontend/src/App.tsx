d402f1b0ac8028cce263ce528c30ac5a9fbbb49d
tonylxm <126369686+tonylxm@users.noreply.github.com>
2025-08-12 17:27:45 +1200

    refactor: Revamped landing and added fade in animation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,23 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Recorder from "./components/Recorder";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Interview Response Analyzer</h1>
-        <p>Practice with random behavioral questions and get AI-powered feedback on your responses</p>
+    <div className="s">
+      <header className="app-header fade-in">
+        <h1>Interview Response Analyser</h1>
+        <p>
+          Practice with random behavioral questions and get
+          <span className="highlight"> AI-powered feedback </span>
+          on your responses.
+        </p>
       </header>
-      <main>
+
+      <main className="fade-in delay">
         <Recorder />
+        <Footer />
       </main>
     </div>
   );
 }
-
-export default App;
